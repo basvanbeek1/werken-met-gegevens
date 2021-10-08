98fb3b0e21608025511b47b415dafc2e76ec2517
basvanbeek1 <99069932@mydavinci.nl>
2021-10-08 13:17:32 +0200

    werken met gegevens toevoegen

---
+++ feestlunch.py
@@ -0,0 +1,3 @@
+kosten = 17 * 0.39 + 2 * 2.78
+korting = 3 * 0.50 
+print ("De kosten van de feest lunch zijn " + str(kosten-korting))
---
+++ speelhaldag.py
@@ -0,0 +1,3 @@
+kostenentree = 7.45 
+extrakosten = (45/5)*0.37
+print("totaal bedrag entree " + str(kostenentree+extrakosten))
